e87172dc9e4f1a63e650c3c8db9e76fd58596fcb
abyss <joemomanugget@gmail.com>
2026-08-15 21:08:07 -0700

    1/1

--- configs/veil/profiledef.sh
+++ configs/veil/profiledef.sh
@@ -11,7 +11,7 @@ buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
